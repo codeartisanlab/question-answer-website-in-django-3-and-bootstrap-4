1a975e32b304676596bbb30cfb2e504b0a7c307d
unknown <codeartisanlab2607@gmail.com>
2020-09-19 00:20:45 +0530

    fetch questions, setup pagination and implement search on home page

--- main/views.py
+++ main/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
-
+from .models import Question
+from django.core.paginator import Paginator
 # Home Page
 def home(request):
-    return render(request,'home.html')
+    if 'q' in request.GET:
+        q=request.GET['q']
+        quests=Question.objects.filter(title__icontains=q).order_by('-id')
+    else:
+        quests=Question.objects.all().order_by('-id')
+    paginator=Paginator(quests,1)
+    page_num=request.GET.get('page',1)
+    quests=paginator.page(page_num)
+    return render(request,'home.html',{'quests':quests})
