ae83aae0480d8e7d2e98222fac3e8083ff2a38fb
unknown <codeartisanlab2607@gmail.com>
2020-09-21 23:28:10 +0530

    Till Add Comment via jquery ajax - Part 7 youtube

--- main - Copy/urls - Copy.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns=[
-    path('',views.home,name='home'),
-    path('detail/<int:id>',views.detail,name='detail'),
-    path('save-comment',views.save_comment,name='save-comment'),
-]
--- main - Copy/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns=[
-    path('',views.home,name='home'),
-    path('detail/<int:id>',views.detail,name='detail'),
-]
